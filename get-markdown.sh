299d31a644577a1c465685cab4f693aed7dab71f
Matt Radford <mradford@protonmail.com>
2020-07-13 17:05:02 -0700

    Remove the first entry from rss

--- get-markdown.sh
+++ get-markdown.sh
@@ -3,11 +3,11 @@
 echo -e 'Yo\n\n' > README.md
 echo -e '<table><tr><td valign="top">\n' >> README.md
 echo -e "### Articles" >> README.md
-curl --silent https://soapstone.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://soapstone.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td><td valign="top">\n' >> README.md
 echo -e "### Book Reviews" >> README.md
-curl --silent https://books.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://books.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td><td valign="top">\n' >> README.md
 echo -e "### Film Reviews" >> README.md
-curl --silent https://film.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://film.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td></tr></table>\n' >> README.md
