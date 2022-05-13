5ef32a096938bcac93b000309de3e8fcf9573fcd
Matt Radford <mradford@protonmail.com>
2022-05-12 17:18:07 -0700

    Update jq selectors

--- get-markdown.sh
+++ get-markdown.sh
@@ -6,11 +6,11 @@ echo "\`\`\`" >> README.md
 echo -e '\n\n' >> README.md
 echo -e '<table><tr><td valign="top">\n' >> README.md
 echo -e "### Articles" >> README.md
-curl --silent https://soapstone.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://soapstone.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link) | select(.link | (contains("books") or contains("film")) | not)][1:][] | "[" +.title+ "](" +.link+ ")"' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td><td valign="top">\n' >> README.md
 echo -e "### Book Reviews" >> README.md
-curl --silent https://books.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://soapstone.mradford.com/rss.xml | xq -r '..|objects|select(.title and .link) | select(.link | contains("books")) | "[" +.title+ "](" +.link+ ")"' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td><td valign="top">\n' >> README.md
 echo -e "### Film Reviews" >> README.md
-curl --silent https://film.mradford.com/rss.xml | xq -r '[..|objects|select(.title and .link)] | .[1:] | map("["+ .title + "](" + .link + ")") | .[]' | xargs -I '{}' echo "- {}" >> README.md
+curl --silent https://soapstone.mradford.com/rss.xml | xq -r '..|objects|select(.title and .link) | select(.link | contains("film")) | "[" +.title+ "](" +.link+ ")"' | xargs -I '{}' echo "- {}" >> README.md
 echo -e '</td></tr></table>\n' >> README.md
